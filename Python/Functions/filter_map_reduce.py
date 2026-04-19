92209538a9d1e7b30b050235fccefbc89bfb6621
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-19 08:41:56 +0530

    Replace is_even lambda with inline lambda in filter function for even number extraction

--- Python/Functions/filter_map_reduce.py
+++ Python/Functions/filter_map_reduce.py
@@ -9,9 +9,10 @@ nums = [1,2,3,4,5,6,7,8,9]
 # def is_even(n):
 #     return  n % 2 == 0
 
-is_even = lambda n: n % 2 == 0
+# is_even = lambda n: n % 2 == 0
 
-evens = list(filter(is_even, nums))  # filter takes a function and an iterable and returns an iterator that produces those items of the iterable for which the function returns true. In this case, we are using the is_even function to filter out the even numbers from the nums list.
+
+evens = list(filter(lambda n: n % 2 == 0, nums))  # filter takes a function and an iterable and returns an iterator that produces those items of the iterable for which the function returns true. In this case, we are using the is_even function to filter out the even numbers from the nums list.
 
 
 print(evens)
