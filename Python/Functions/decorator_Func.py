cee3d5dc6e0bdecf335ca16f35d1906679f8764e
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-19 09:17:44 +0530

    Remove decorator syntax from function definitions and apply decorator explicitly

--- Python/Functions/decorator_Func.py
+++ Python/Functions/decorator_Func.py
@@ -6,13 +6,17 @@ def greater_first(func): #decorator
         return func(a,b) #call the original function
     return wrap  #return the wrapper function
 
-@greater_first #decorator
+# @greater_first #decorator
 def divide(a,b):
     return a/b
 
-@greater_first #decorator
+# @greater_first #decorator
 def subtract(a,b):
     return a-b
+
+
+divide = greater_first(divide) #we can also use the decorator by calling the greater_first function and passing the divide function as an argument. This will return the wrapper function which we can assign to the divide variable.
+subtract = greater_first(subtract) #we can also use the decorator by calling the greater_first function and passing the subtract function as an argument. This will return the wrapper function which we can assign to the subtract variable.
 
 result1 = divide(4,2)
 print(result1)
