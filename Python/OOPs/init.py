af127363a9f75e7a281d6cc66b440408470a26a1
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-20 08:24:47 +0530

    Refactor computer class to accept parameters in constructor and update config method for dynamic output

--- Python/OOPs/init.py
+++ Python/OOPs/init.py
@@ -2,19 +2,22 @@
 class computer:
 
 
-    def __init__(self): # constructor method
+    def __init__(self, cpu, ram, hdd, vram, gpu): # constructor method
         print('init method called') # this method will be called automatically when we create an object of the class
-        self.cpu = 'i7'
+        self.cpu = cpu
+        self.ram = ram
+        self.hdd = hdd
+        self.vram = vram
+        self.gpu = gpu
 
 
     def config(self):
-        print("i7, 32GB, 1TB, 16GB VRAM, NVIDIA RTX 4060")
+        print('config :', self.cpu, self.ram, self.hdd, self.vram, self.gpu) # this method will print the configuration of the computer
 
 
-comp1 = computer()
-comp2 = computer()
+comp1 = computer('i5', '16GB', '512GB', '8GB VRAM', 'NVIDIA GTX 4050') # creating an object of the class computer and passing the configuration as arguments
+comp2 = computer('i9', '64GB', '2TB', '32GB VRAM', 'NVIDIA RTX 4090') # creating another object of the class computer and passing the configuration as arguments
 
-print(comp1.cpu) # accessing instance variable using object name
 
 comp1.config() # calling method using object name
 comp2.config() # calling method using object name
