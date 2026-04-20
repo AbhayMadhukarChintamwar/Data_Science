9efa323a6986a35d70b02f1019be5d9291efd515
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-20 12:17:59 +0530

    Add exception handling

--- Python/Advanced/exception.py
+++ Python/Advanced/exception.py
@@ -1,12 +1,16 @@
 
-a = 5
-b = 0    # ZeroDivisionError: division by zero
+# a = 5
+# b = 0   # ZeroDivisionError: division by zero
 
 try:
+
+    a = int(input("Enter the value of a : "))
+    b = int(input("Enter the value of b : "))
     result = a / b
     print(result)
-except ZeroDivisionError:
-    print("Error: Division by zero is not allowed.")
+
+except Exception as e:
+    print("An error occurred : ",e)
 
 print('End of the execution')
 
