cc22aa6157f2db69ef3cc7fb9513f7b30b99a8be
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-21 10:18:05 +0530

    Refactor NumPy example to include detailed comments and improve output clarity

--- NumPy/Basic.py
+++ NumPy/Basic.py
@@ -1,8 +1,9 @@
 import numpy as np
-mat = [1,2,3,'gre',1.5]
-matrix = np.array([1,2,3,'gre',1.5])
+mat = [1,2,3,'gre',1.5] # When we create a numpy array with mixed data types, it will upcast to the most general type that can accommodate all the values. In this case, since we have a string 'gre', the entire array will be upcast to a string type.
+matrix = np.array([1,2,3,'gre',1.5]) # The resulting array will contain all elements as strings: ['1', '2', '3', 'gre', '1.5']. This is because numpy arrays require all elements to be of the same type, and the presence of a string forces the entire array to be treated as strings.
 
-print(type(mat))
-print(mat)
-print(type(matrix))
-print(matrix)
+print(type(mat)) # Output: <class 'list'>
+print(mat) # Output: <class 'list'> and [1, 2, 3, 'gre', 1.5]
+print(type(matrix)) # Output: <class 'numpy.ndarray'>
+print(matrix) # Output: ['1' '2' '3' 'gre' '1.5']
+
