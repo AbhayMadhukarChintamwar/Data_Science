f7f4162f4d33d72d87726e2b69b656708956e4ad
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-22 12:51:36 +0530

    Refactor read_file.py to uncomment data analysis prints and enhance dataset insights

--- Pandas/Basic_Pandas/read_file.py
+++ Pandas/Basic_Pandas/read_file.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
 df = pd.read_csv("finance_small_data.csv")
-print(df.head())
-print(df.info())
-print(df.shape)
-print(df.loc[0:99, "Date"])
-print(df.loc[:, "Transaction_Type"])
+# print(df.head())
+# print(df.info())
+# print(df.shape)
+# print(df.loc[0:99, "Date"])
+# print(df.loc[:, "Transaction_Type"])
+
+# What is the highest price in the dataset?
+max_Price = df['Price'].max()
+print(max_Price)
+print(df.max())
+print(df.min())
+print(df['Price'].mean())
