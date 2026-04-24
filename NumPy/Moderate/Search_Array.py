009e7b5c70e325ff8d1a7065fd008957ea6f2585
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-24 15:40:41 +0530

    Add examples for searching and sorting arrays in Search_Array.py

--- NumPy/Moderate/Search_Array.py
+++ NumPy/Moderate/Search_Array.py
@@ -14,3 +14,36 @@ search_arrays = np.where(arr%2==0)
 print(search_arrays)  #  (array([1, 3, 5, 7]),)
 
 
+# Search Sorted Array
+arr = np.array([1,2,4,5,6,7,8,9])
+
+
+search_sorted_array = np.searchsorted(arr,3 ,side='right')
+print(search_sorted_array)  # 2
+
+# Sorted Array
+
+arr = np.array([1,22,12,33,23,5,6,7,8,9])
+sorted_array =np.sort(arr)
+print(sorted_array)  # [ 1  5  6  7  8  9 12 22 23 33]
+
+
+arr = np.array([[1,22,12,33,23],[5,6,77,8,49]])
+sorted_array =np.sort(arr)
+print(sorted_array)
+            # [[ 1 12 22 23 33]
+            #  [ 5  6  8 49 77]]
+
+
+arr = np.array(['abhay','kittu','amar','swanand','praful','avinash'])
+sorted_array =np.sort(arr)
+print(sorted_array)  # ['abhay' 'amar' 'avinash' 'kittu' 'praful' 'swanand']
+
+
+arr = np.array([['abhay','kittu','amar'],['swanand','praful','avinash']])
+sorted_array =np.sort(arr)
+print(sorted_array)  # ['abhay' 'amar' 'avinash' 'kittu' 'praful' 'swanand']
+
+
+
+
