4001c48fea43ad6f5bcb69897208da5307244778
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-25 08:29:30 +0530

    Add examples for the insert function in Filter_Array.py

--- NumPy/Moderate/Filter_Array.py
+++ NumPy/Moderate/Filter_Array.py
@@ -39,3 +39,25 @@ print(np.resize(arr,(2,3)))
 
 arr = np.array([5,32,32,44,5,55])
 print(np.insert(arr,3,60))  # [ 5 32 32 60 44  5 55]
+
+
+arr = np.array([5,32,32,44,5,55])
+print(np.insert(arr,(3,4),(70,80)))  # [ 5 32 32 44 70  5 80 55]
+
+arr_2d = np.array([[5,32,32,44,5,55],[45,42,3,4,54,65]])
+print(arr_2d)
+            # [ 5 32 32 60 44  5 55]
+            # [ 5 32 32 70 44 80  5 55]
+
+
+insert_2d = np.insert(arr_2d,1,100,axis =0)
+print(insert_2d)
+            # [[  5  32  32  44   5  55]
+            #  [100 100 100 100 100 100]
+            #  [ 45  42   3   4  54  65]]
+
+
+insert_2d = np.insert(arr_2d,3,100,axis =1)
+print(insert_2d)
+            # [[  5  32  32 100  44   5  55]
+            #  [ 45  42   3 100   4  54  65]]
