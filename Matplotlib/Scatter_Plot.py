5c935f8c92ae4a13b2d3ed85fd57e4e681c8d430
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-28 11:28:26 +0530

    Add Scatter_Plot.py to create a scatter plot using Matplotlib

---
+++ Matplotlib/Scatter_Plot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# Scatter Plot
+
+x = [1,2,3,4,5,6]
+y = [4,2,3,5,1,8]
+plt.title('Scatter Plot', fontsize = 20)
+plt.xlabel('Month')
+plt.ylabel('Number')
+# plt.scatter(x,y, color ='r')
+plt.scatter(x,y, color ='r')
+
+print(plt.show())
