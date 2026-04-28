c18354535cc24afdd37124cdd06f12aaa95b257d
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-04-28 12:21:36 +0530

    Change scatter plot colormap to 'Dark2' and add colorbar; print show() output.

--- Matplotlib/Scatter_Plot.py
+++ Matplotlib/Scatter_Plot.py
@@ -10,5 +10,9 @@ plt.ylabel('Number')
 # plt.scatter(x,y, color ='r')
 size = [300,170, 350, 264, 209, 219]
 c = [10,20,30,45,78,99]
-plt.scatter(x,y, c = c , s=size , cmap ='Accent')
-plt.show()
+plt.scatter(x,y, c = c , s=size , cmap ='Dark2')
+plt.colorbar()
+
+
+
+print(plt.show())
