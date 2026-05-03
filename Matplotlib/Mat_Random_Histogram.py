f248caefb268df2d21503e139f99ead15de8f410
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-05-03 08:37:18 +0530

    Add initial implementation of random histogram generation and plotting

---
+++ Matplotlib/Mat_Random_Histogram.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+random_Histogram = np.random.randint(10, 40, (20))
+print(random_Histogram)
+num = random_Histogram
+print()
+print(num)
